61b2f63cbbc956afd2127e17772f13a079ea2345
Pedram Hamidehkhan <pedram.hamidehkhan@gmail.com>
2021-11-25 15:57:59 +0100

    api gateway output removed

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,7 @@
-# output "function_name" {
-#   description = "Name of the Lambda function."
 
-#   value = aws_lambda_function.hello_world.function_name
+# //output the api gateways url
+# output "api_url" {
+#   description = "URL of the API Gateway."
+
+#   value = aws_apigatewayv2_api.terraapi.api_endpoint
 # }
-
-//output the api gateways url
-output "api_url" {
-  description = "URL of the API Gateway."
-
-  value = aws_apigatewayv2_api.terraapi.api_endpoint
-}
