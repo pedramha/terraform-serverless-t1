5213cea27cfd03bfa37652cb36b2d13ec26aca66
Pedram Hamidehkhan <pedram.hamidehkhan@gmail.com>
2021-11-28 23:44:12 +0100

    step f

--- modules/business/main.tf
+++ modules/business/main.tf
@@ -1,28 +1,16 @@
-resource "aws_sfn_state_machine" "my_state_machine" {
-  name = "my_state_machine"
-  //arn:aws:lambda:eu-central-1:833915806704:function:HelloWorld
-  role_arn = aws_iam_role.iam_for_step_function.arn
-  definition = file("stepfunc.json.asl")
-   tags = {
-    "env" = "dev"
-  }
-}
+# resource "aws_sfn_state_machine" "my_state_machine" {
+#   name = "my_state_machine"
+#   role_arn = aws_iam_role.iam_for_step_function.arn
+#   definition = file("stepfunc.json.asl")
+#    tags = {
+#     "env" = "dev"
+#   }
+# }
 
-resource "aws_iam_role" "iam_for_step_function" {
-  name = "iam_for_step_function"
-  assume_role_policy = file("stepfuncpolicy.json")
-   tags = {
-    "env" = "dev"
-  }
-  # jsonencode({
-  #   "Version" : "2012-10-17",
-  #   "Statement" : [{
-  #     "Action" : "sts:AssumeRole",
-  #     "Principal" : {
-  #       "Service" : "states.amazonaws.com"
-  #     },
-  #     "Effect" : "Allow",
-  #     "Sid" : ""
-  #   }]
-  # })
-}
+# resource "aws_iam_role" "iam_for_step_function" {
+#   name = "iam_for_step_function"
+#   assume_role_policy = file("stepfuncpolicy.json")
+#    tags = {
+#     "env" = "dev"
+#   }
+# }
