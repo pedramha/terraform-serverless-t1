f418ad82782cc6c1117cf8e19d774744d8d7599c
Pedram Hamidehkhan <pedram.hamidehkhan@gmail.com>
2021-11-28 17:58:23 +0100

    s3

--- modules/frontend/main.tf
+++ modules/frontend/main.tf
@@ -2,21 +2,8 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-module "aws_static_website" {
-  source = "./${path.module}/static"
-
-  website-domain-main     = "pedkopp92iasd.com"
-  website-domain-redirect = "www.pedkopp92iasd.com"
-  policy                  = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "PublicRead",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": "s3:GetObject",
-        "Resource": "arn:aws:s3:::${module.aws_static_website.website-domain-main}/*"
-      }
-    ]
-  })
+//s3 bucket for static web hosting
+resource "aws_s3_bucket" "static" {
+  bucket = "pedkopp92iasd.com"
+  acl    = "public-read"
 }
