7f6490ce2f3fa013000dd13390bbfb19ed8df7b7
Pedram Hamidehkhan <pedram.hamidehkhan@gmail.com>
2021-11-28 23:48:00 +0100

    stepf

--- modules/business/main.tf
+++ modules/business/main.tf
@@ -1,16 +1,16 @@
-# resource "aws_sfn_state_machine" "my_state_machine" {
-#   name = "my_state_machine"
-#   role_arn = aws_iam_role.iam_for_step_function.arn
-#   definition = file("stepfunc.json.asl")
-#    tags = {
-#     "env" = "dev"
-#   }
-# }
+resource "aws_sfn_state_machine" "my_state_machine" {
+  name = "my_state_machine"
+  role_arn = aws_iam_role.iam_for_step_function.arn
+  definition = file("stepfunc.json.asl")
+   tags = {
+    "env" = "dev"
+  }
+}
 
-# resource "aws_iam_role" "iam_for_step_function" {
-#   name = "iam_for_step_function"
-#   assume_role_policy = file("stepfuncpolicy.json")
-#    tags = {
-#     "env" = "dev"
-#   }
-# }
+resource "aws_iam_role" "iam_for_step_function" {
+  name = "iam_for_step_function"
+  assume_role_policy = file("stepfuncpolicy.json")
+   tags = {
+    "env" = "dev"
+  }
+}
