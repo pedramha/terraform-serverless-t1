d54c3138fb76215d376118ff1f75cd0f9debc741
Pedram Hamidehkhan <pedram.hamidehkhan@gmail.com>
2021-11-28 17:52:59 +0100

    frontend path amended

--- modules/frontend/main.tf
+++ modules/frontend/main.tf
@@ -3,9 +3,20 @@ provider "aws" {
 }
 
 module "aws_static_website" {
-  source = "${path.module}/static"
+  source = "./${path.module}/static"
 
   website-domain-main     = "pedkopp92iasd.com"
   website-domain-redirect = "www.pedkopp92iasd.com"
+  policy                  = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "PublicRead",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "s3:GetObject",
+        "Resource": "arn:aws:s3:::${module.aws_static_website.website-domain-main}/*"
+      }
+    ]
+  })
 }
-
