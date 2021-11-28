7b47c6825aff97a4a0f442d880be0306273e715b
Pedram Hamidehkhan <pedram.hamidehkhan@gmail.com>
2021-11-28 18:06:52 +0100

    s3 added

--- modules/frontend/main.tf
+++ modules/frontend/main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 //s3 bucket for static web hosting
-# resource "aws_s3_bucket" "static" {
-#   bucket = "pedkopp92iasd.com"
-#   acl    = "public-read"
-#     provisioner "local-exec" {
-#      command = "aws s3 cp ./static ${aws_s3_bucket.static.id}"
-#   }
-# }
+resource "aws_s3_bucket" "static" {
+  bucket = "pedkopp92iasd.com"
+  acl    = "public-read"
+    provisioner "local-exec" {
+     command = "aws s3 cp ./static ${aws_s3_bucket.static.id}"
+  }
+}
