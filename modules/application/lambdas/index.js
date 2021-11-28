9ad4bca1fbed8c9471db0e949d5d258b84bc4d0f
Pedram Hamidehkhan <pedram.hamidehkhan@gmail.com>
2021-11-28 13:04:58 +0100

    lambda

--- modules/application/lambdas/index.js
+++ modules/application/lambdas/index.js
@@ -1,17 +1,49 @@
+const AWS = require('aws-sdk');
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
 module.exports.handler = async (event) => {
   console.log('Event: ', event);
-  let responseMessage = 'Hello, World!';
+
+  let body = {"test":"1"};
+  let statusCode = '200';
+  const headers = {
+      'Content-Type': 'application/json',
+  };
+
+  try {
+    body = await dynamo.put(JSON.parse(body)).promise();
+      // switch (event.httpMethod) {
+      //     case 'DELETE':
+      //         body = await dynamo.delete(JSON.parse(event.body)).promise();
+      //         break;
+      //     case 'GET':
+      //         body = await dynamo.scan({ TableName: event.queryStringParameters.TableName }).promise();
+      //         break;
+      //     case 'POST':
+      //         body = await dynamo.put(JSON.parse(event.body)).promise();
+      //         break;
+      //     case 'PUT':
+      //         body = await dynamo.update(JSON.parse(event.body)).promise();
+      //         break;
+      //     default:
+              // throw new Error(`Unsupported method "${event.httpMethod}"`);
+      // }
+  } catch (err) {
+      statusCode = '400';
+      body = err.message;
+  } finally {
+      body = JSON.stringify(body);
+  }
 
   return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      message: responseMessage,
-    }),
-  }
-}
+      statusCode,
+      body,
+      headers,
+  };
+};
+  
+
 // // Load the AWS SDK for Node.js
 // var AWS = require('aws-sdk');
 // // Set the region 
