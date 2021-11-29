3a6304cb212859a5de3c41d3e2af57a5f0c0799f
Pedram Hamidehkhan <pedram.hamidehkhan@gmail.com>
2021-11-29 12:34:31 +0100

    removed comments

--- modules/frontend/main.tf
+++ modules/frontend/main.tf
@@ -1,22 +1,3 @@
 provider "aws" {
   region = "eu-central-1"
 }
-
-# resource "aws_s3_bucket" "static" {
-#   bucket = "pedramterraformsdemo.com"
-#   acl    = "public-read"
-
-#   provisioner "local-exec" {
-#     command = "aws s3 sync static/ s3://pedramterraformsdemo.com --acl public-read --delete"
-#   }
-
-#   website {
-#     index_document = "index.html"
-#   }
-
-#   # versioning {
-#   #   enabled = true
-#   # }
-
-
-# }
