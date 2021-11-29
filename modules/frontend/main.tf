dfa44bcecc68768e7378ecfe4dcac14b63121afe
Pedram Hamidehkhan <pedram.hamidehkhan@gmail.com>
2021-11-29 12:10:20 +0100

    remove s3

--- modules/frontend/main.tf
+++ modules/frontend/main.tf
@@ -1,22 +1,22 @@
-provider "aws" {
-  region = "eu-central-1"
-}
+# provider "aws" {
+#   region = "eu-central-1"
+# }
 
-resource "aws_s3_bucket" "static" {
-  bucket = "pedramterraformsdemo.com"
-  acl    = "public-read"
+# resource "aws_s3_bucket" "static" {
+#   bucket = "pedramterraformsdemo.com"
+#   acl    = "public-read"
 
-  provisioner "local-exec" {
-    command = "aws s3 sync static/ s3://pedramterraformsdemo.com --acl public-read --delete"
-  }
+#   provisioner "local-exec" {
+#     command = "aws s3 sync static/ s3://pedramterraformsdemo.com --acl public-read --delete"
+#   }
 
-  website {
-    index_document = "index.html"
-  }
+#   website {
+#     index_document = "index.html"
+#   }
 
-  # versioning {
-  #   enabled = true
-  # }
+#   # versioning {
+#   #   enabled = true
+#   # }
 
 
-}
+# }
