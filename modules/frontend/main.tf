c5338cdf9fdc4fcfaac39c3bf93f44f4dc3e70ef
Pedram Hamidehkhan <pedram.hamidehkhan@gmail.com>
2021-11-29 13:42:10 +0100

    frontend added

--- modules/frontend/main.tf
+++ modules/frontend/main.tf
@@ -1,3 +1,17 @@
 provider "aws" {
   region = "eu-central-1"
 }
+
+resource "aws_s3_bucket" "static" {
+  bucket = "pedramterraformexercie.com"
+  acl    = "public-read"
+  
+  provisioner "local-exec" {
+    command = "aws s3 sync static/ s3://pedramterraformexercie.com --acl public-read --delete"
+  }
+
+website {
+  index_document = "index.html"
+}
+
+}
