c9f6cdf90648f5664fa6e798502338a425d7a5c6
Pedram Hamidehkhan <pedram.hamidehkhan@gmail.com>
2021-11-29 11:43:39 +0100

    address amended addres

--- modules/business/main.tf
+++ modules/business/main.tf
@@ -1,7 +1,7 @@
 resource "aws_sfn_state_machine" "my_state_machine" {
   name = "my_state_machine"
   role_arn = aws_iam_role.iam_for_step_function.arn
-  definition = file("stepfunc.json.asl")
+  definition = file("${path.module}/stepfunc.json.asl")
    tags = {
     "env" = "dev"
   }
@@ -9,7 +9,7 @@ resource "aws_sfn_state_machine" "my_state_machine" {
 
 resource "aws_iam_role" "iam_for_step_function" {
   name = "iam_for_step_function"
-  assume_role_policy = file("stepfuncpolicy.json")
+  assume_role_policy = file("${path.module}/stepfuncpolicy.json")
    tags = {
     "env" = "dev"
   }
