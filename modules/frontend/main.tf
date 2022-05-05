7b5de2be389eeb880b61e155d40ca5437f381392
pedram.hamidehkhan <pedram.hamidehkhan@hashicorp.com>
2022-05-05 14:29:17 +0200

    changed to public bucket config

--- modules/frontend/main.tf
+++ modules/frontend/main.tf
@@ -7,30 +7,30 @@ locals {
   region      = "eu-west-1"
 }
 
-module "s3-bucket" {
-  source  = "app.terraform.io/pedram-company/s3-bucket/aws"
-  tags{
-    Owner = "pedramhamidehkhan"
-  }
+# module "s3-bucket" {
+#   source  = "app.terraform.io/pedram-company/s3-bucket/aws"
+#   tags = {
+#     Owner = "pedramhamidehkhan"
+#   }
+#   acl    = "public-read"
+#     provisioner "local-exec" {
+#     command = "aws s3 sync static/ s3:/${bucket_name}.com --acl public-read --delete"
+#   }
+# website {
+#   index_document = "index.html"
+# }
+# }
+
+resource "aws_s3_bucket" "static" {
+  bucket = "pedramterraformexercie.com"
   acl    = "public-read"
-    provisioner "local-exec" {
-    command = "aws s3 sync static/ s3:/${bucket_name}.com --acl public-read --delete"
+  
+  provisioner "local-exec" {
+    command = "aws s3 sync static/ s3://${bucket_name}.com --acl public-read --delete"
   }
+
 website {
   index_document = "index.html"
 }
 
 }
-# resource "aws_s3_bucket" "static" {
-#   bucket = "pedramterraformexercie.com"
-#   acl    = "public-read"
-  
-#   provisioner "local-exec" {
-#     command = "aws s3 sync static/ s3://pedramterraformexercie.com --acl public-read --delete"
-#   }
-
-# website {
-#   index_document = "index.html"
-# }
-
-# }
