e8794fde8175baca23ca423ad002f805b75c0e4a
pedram.hamidehkhan <pedram.hamidehkhan@hashicorp.com>
2022-05-05 14:12:01 +0200

    changed bucket to private registry

--- modules/frontend/main.tf
+++ modules/frontend/main.tf
@@ -1,17 +1,33 @@
-provider "aws" {
-  region = "eu-central-1"
+locals {
+  bucket_name = "s3-bucket-pedram-company-website"
+  region      = "eu-west-1"
 }
 
-resource "aws_s3_bucket" "static" {
-  bucket = "pedramterraformexercie.com"
-  acl    = "public-read"
-  
-  provisioner "local-exec" {
-    command = "aws s3 sync static/ s3://pedramterraformexercie.com --acl public-read --delete"
+module "s3-bucket" {
+  source  = "app.terraform.io/pedram-company/s3-bucket/aws"
+  version = "3.2.0"
+  tags{
+    Owner = "pedramhamidehkhan"
   }
-
+  acl    = "public-read"
+    provisioner "local-exec" {
+    command = "aws s3 sync static/ s3:/${bucket_name}.com --acl public-read --delete"
+  }
 website {
   index_document = "index.html"
 }
 
 }
+# resource "aws_s3_bucket" "static" {
+#   bucket = "pedramterraformexercie.com"
+#   acl    = "public-read"
+  
+#   provisioner "local-exec" {
+#     command = "aws s3 sync static/ s3://pedramterraformexercie.com --acl public-read --delete"
+#   }
+
+# website {
+#   index_document = "index.html"
+# }
+
+# }
