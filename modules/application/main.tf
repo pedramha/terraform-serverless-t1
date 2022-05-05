2802feec747096793378e8bd4c36074f95fad6da
pedram.hamidehkhan <pedram.hamidehkhan@hashicorp.com>
2022-05-05 18:45:12 +0200

    amended api

--- modules/application/main.tf
+++ modules/application/main.tf
@@ -173,7 +173,7 @@ resource "aws_api_gateway_method" "apimethod" {
 resource "aws_api_gateway_integration" "get_integration" {
   rest_api_id             = aws_api_gateway_rest_api.restapi.id
   resource_id             = aws_api_gateway_resource.apiresource.id
-  http_method             = aws_api_gateway_method.apimethod
+  http_method             = aws_api_gateway_method.apimethod.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.crud.invoke_arn
