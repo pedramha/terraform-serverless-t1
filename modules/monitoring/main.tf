00c3c1c2709184803ccf169ead4934501d3edd85
pedram.hamidehkhan <pedram.hamidehkhan@hashicorp.com>
2022-05-06 09:32:31 +0200

    added lambda for monitoring

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -50,6 +50,59 @@ resource "aws_cloudwatch_dashboard" "lambdadash" {
                     {
                         "key": "owner",
                         "value": "pedram@hashicorp.com"
+                    },
+                    {
+                        "key": "env",
+                        "value": "dev"
+                    }
+                ],
+                "widgetOptions": {
+                    "legend": {
+                        "position": "bottom"
+                    },
+                    "view": "timeSeries",
+                    "stacked": false,
+                    "rowsPerPage": 50,
+                    "widgetsPerRow": 2
+                },
+                "period": 300,
+                "splitBy": "",
+                "region": "eu-west-1"
+            }
+        },
+        {
+            "type": "explorer",
+            "x": 0,
+            "y": 15,
+            "width": 24,
+            "height": 15,
+            "properties": {
+                "metrics": [
+                    {
+                        "metricName": "Invocations",
+                        "resourceType": "AWS::Lambda::Function",
+                        "stat": "Sum"
+                    },
+                    {
+                        "metricName": "Duration",
+                        "resourceType": "AWS::Lambda::Function",
+                        "stat": "Average"
+                    },
+                    {
+                        "metricName": "Errors",
+                        "resourceType": "AWS::Lambda::Function",
+                        "stat": "Sum"
+                    },
+                    {
+                        "metricName": "Throttles",
+                        "resourceType": "AWS::Lambda::Function",
+                        "stat": "Sum"
+                    }
+                ],
+                "labels": [
+                    {
+                        "key": "owner",
+                        "value": "pedram@hashicorp.com"
                     }
                 ],
                 "widgetOptions": {
